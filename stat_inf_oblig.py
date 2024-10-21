76ba595b086fad703c52dc8898904168d3ed84a6
CF <carlfan@uio.no>
2024-10-21 14:07:37 +0200

    Ferdig med 1-3

--- stat_inf_oblig.py
+++ stat_inf_oblig.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import rcParams, colors, cm, colormaps
+from matplotlib.cm import ScalarMappable
 from scipy import stats
 
 from matplotlib import rcParams
@@ -117,6 +118,73 @@ def confidence_interval(mu_est, sigma_square_est, n_df):
     return np.array([lower_limit * sigma_hat / np.sqrt(n) + mu_est, upper_limit * sigma_hat / np.sqrt(n) + mu_est])
 
 N = 10**6
-mu_est, sigma_square_est = estimate_e(N)
-print("Estimation for e: {}, with variance estimate {}".format(mu_est, sigma_square_est))
-print("99% confidence interval for e: [{}, {}]".format(*confidence_interval(mu_est, sigma_square_est, N - 1)))
+# mu_est, sigma_square_est = estimate_e(N)
+# print("Estimation for e: {}, with variance estimate {}".format(mu_est, sigma_square_est))
+# print("99% confidence interval for e: [{}, {}]".format(*confidence_interval(mu_est, sigma_square_est, N - 1)))
+
+def sample_random_normal_triplet(n):
+    """Sampling n triplets of random variables (X, Y, Z), where X, Y, Z are independent standard normals."""
+    return np.random.normal(size=(n, 3))
+
+def spherical_transform(x_y_zs):
+    """Takes in an array of shape (n, 3) and transforms each triplet to spherical coordinates (R, A, B).
+    A in [0, pi]."""
+
+    def spherical(x, y, z):
+        r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+        return np.array([r, np.arccos(z / r), np.sign(y) * np.arccos(x / np.sqrt(x**2 + y ** 2))])
+
+    r_a_bs = np.zeros_like(x_y_zs)
+    for i in range(x_y_zs.shape[0]):
+        r_a_bs[i] = spherical(*x_y_zs[i, :])
+    return r_a_bs
+
+# rand_samples = sample_random_normal_triplet(10)
+# rand_spher = spherical_transform(rand_samples)
+# print(rand_samples)
+# print(rand_spher)
+
+
+def plot_histograms(n):
+    x_y_zs = sample_random_normal_triplet(n)
+    r_a_bs = spherical_transform(x_y_zs)
+    # print(x_y_zs)
+    # print(r_a_bs)
+    fig, axs = plt.subplots(ncols=3, figsize=(5.8, 2.3))
+    colornorm = colors.Normalize(vmin=0, vmax=2)
+    cmap = colormaps["viridis"]
+    num_to_col = cm.ScalarMappable(norm=colornorm, cmap=cmap)
+    linspace1 = np.linspace(0, max(r_a_bs[:, 0]), 1000)
+    linspace2 = np.linspace(0, np.pi, 1000)
+    linspace3 = np.linspace(-np.pi, np.pi, 100)
+    rs = np.sqrt(2 / np.pi) * linspace1 ** 2 * np.exp(-linspace1**2 / 2)
+    a_arr = np.sin(linspace2) / 2
+    b_arr = np.array([1 / (2 * np.pi) for i in linspace3])
+    linspaces = [linspace1, linspace2, linspace3]
+    densities = [rs, a_arr, b_arr]
+    for i, (ax, linspace, density) in enumerate(zip(axs, linspaces, densities)):
+        ax.hist(r_a_bs[:, i], bins='auto', color=num_to_col.to_rgba(i), density=True)
+        ax.plot(linspace, density, color="black", label=r"$g_{}$".format(i+1))
+        ax.grid()
+        ax.legend()
+    axs[0].set_xlim(0, max(r_a_bs[:, 0]))
+    axs[0].set_title(r"$R$")
+    axs[0].set_xlabel(r"$r$")
+    axs[0].set_xticks(np.array([0, 1, 2, 3, 4]))
+
+    axs[1].set_xlim(0, np.pi)
+    axs[1].set_title(r"$A$")
+    axs[1].set_xlabel(r"$a$")
+    axs[1].set_xticks(np.pi * np.array([0, 1 / 4, 1/2, 3/4, 1]))
+    axs[1].set_xticklabels([0, r"$\frac{\pi}{4}$", r"$\frac{\pi}{2}$", r"$\frac{3\pi}{4}$", r"$\pi$"])
+
+    axs[2].set_xlim(-np.pi, np.pi)
+    axs[2].set_title(r"$B$")
+    axs[2].set_xlabel(r"$b$")
+    axs[2].set_xticks(np.pi * np.array([-1, -2 / 3, -1/3, 0, 1/3, 2/3, 1]))
+    axs[2].set_xticklabels([r"$-\pi$", r"$-\frac{2}{3}\pi$", r"$-\frac{1}{3}\pi$", 0 , r"$\frac{1}{3}\pi$", r"$\frac{2}{3}\pi$", r"$\pi$"])
+    plt.tight_layout()
+    plt.savefig(fname="spherically_transformed_triplett.svg")
+    plt.show()
+
+plot_histograms(1000)
